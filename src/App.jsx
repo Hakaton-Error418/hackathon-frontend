180a3c86b89154f7fd7c8f8204c68255bd17751a
Maria <maryallbond@gmail.com>
2025-02-10 12:37:56 +0200

    add light resistration and signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router"
 import { Home } from "./components/pages/Home"
 import { Layout } from "./Layouts/Layout"
-import "./styles/index.css"
+
 
 export const App = () => {
     return (
@@ -10,7 +10,7 @@ export const App = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="/profile" element={<ProfileLayout />}>
+                    {/* <Route path="/profile" element={<ProfileLayout />}>
                         <Route index element={<Profile />} />
                         <Route path="setting" element={<ProfileSetting />} />
                         <Route path="rating" element={<Rating />} />
@@ -26,7 +26,7 @@ export const App = () => {
                         <Route index element={<Test />} />
                         <Route path="itmap/:count" element={<Itmap />} />
                         <Route path="question/:id" element={<Question />} />
-                    </Route>
+                    </Route> */}
                 </Route>
             </Routes>
         </>
--- src/Layouts/Modals/RootModals.jsx
+++ src/Layouts/Modals/RootModals.jsx
@@ -1,6 +1,10 @@
+import Registration from "./registration"
+
 export const RootModals = () => {
     return (
         <>
+            {/* <Registration signin={false} /> */}
+            {/* <Registration signin={true} /> */}
             <h1>Rootmodal</h1>
         </>
     )
