e8828fd4f59af68264bbdf906d87622162b3094d
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2025-02-10 13:05:52 +0200

    dfgdfgdf

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,24 @@ export const App = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    {/* <Route path="/profile" element={<ProfileLayout />}>
-                        <Route index element={<Profile />} />
-                        <Route path="setting" element={<ProfileSetting />} />
+                    {/* <Route path="/profile" element={<ProfileLayout />}> */}
+                        {/* <Route index element={<Profile />} /> */}
+                        {/* <Route path="setting" element={<ProfileSetting />} />
                         <Route path="rating" element={<Rating />} />
                         <Route path="quests" element={<QuestsLayout />}>
                             <Route index element={<Quests />} />
                             <Route path="new" element={<CreateQuests />} />
                         </Route>
                         <Route path="history" element={<History />} />
-                        <Route path="achievements" element={<Achievements />} />
-                    </Route> */}
+                        <Route path="achievements" element={<Achievements />} /> */}
+                    </Route>
                     {/* <Route path="/join/:id" element={<Join />} /> 
                     <Route path="/test" element={<TestLayout />}>
                         <Route index element={<Test />} />
                         <Route path="itmap/:count" element={<Itmap />} />
                         <Route path="question/:id" element={<Question />} />
                     </Route> */}
-                </Route>
+                {/* </Route> */}
             </Routes>
         </>
     )
