1f79d424336c488cf4c74a3e9f13d97151eb26de
Maria <maryallbond@gmail.com>
2025-02-12 14:22:31 +0200

    add google auth

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client"
 import { App } from "./App"
 import { BrowserRouter } from "react-router"
 import "./styles/index.css"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+
+const CLIENT_ID =
+    "1020140561138-eciks6mn38s7d2c90f54k4oorrpsa6id.apps.googleusercontent.com"
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter basename="/">
-        <App />
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+            <App />
+        </GoogleOAuthProvider>
     </BrowserRouter>
 )
